94f0e6ed0e3d8e380ccf1781624b9da75cfbaa57
Jaymian-Lee <jaymian-lee@ziggo.nl>
2024-06-10 10:39:04 +0200

    chore(logos) Created logo & basic styling

---
+++ Gasolin/Landing.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Gasolin
+{
+    partial class Landing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Landing));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnColleague = new System.Windows.Forms.Button();
+            this.btnCars = new System.Windows.Forms.Button();
+            this.btnTank = new System.Windows.Forms.Button();
+            this.lstColleague = new System.Windows.Forms.ListView();
+            this.lstCars = new System.Windows.Forms.ListView();
+            this.lstTank = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnColleague
+            // 
+            this.btnColleague.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColleague.Location = new System.Drawing.Point(340, 41);
+            this.btnColleague.Name = "btnColleague";
+            this.btnColleague.Size = new System.Drawing.Size(136, 54);
+            this.btnColleague.TabIndex = 1;
+            this.btnColleague.Text = "Medewerkers";
+            this.btnColleague.UseVisualStyleBackColor = true;
+            // 
+            // btnCars
+            // 
+            this.btnCars.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCars.Location = new System.Drawing.Point(482, 41);
+            this.btnCars.Name = "btnCars";
+            this.btnCars.Size = new System.Drawing.Size(136, 54);
+            this.btnCars.TabIndex = 2;
+            this.btnCars.Text = "Auto\'s";
+            this.btnCars.UseVisualStyleBackColor = true;
+            // 
+            // btnTank
+            // 
+            this.btnTank.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTank.Location = new System.Drawing.Point(624, 41);
+            this.btnTank.Name = "btnTank";
+            this.btnTank.Size = new System.Drawing.Size(136, 54);
+            this.btnTank.TabIndex = 3;
+            this.btnTank.Text = "Tankbeurt";
+            this.btnTank.UseVisualStyleBackColor = true;
+            // 
+            // lstColleague
+            // 
+            this.lstColleague.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lstColleague.HideSelection = false;
+            this.lstColleague.Location = new System.Drawing.Point(12, 176);
+            this.lstColleague.Name = "lstColleague";
+            this.lstColleague.Size = new System.Drawing.Size(253, 262);
+            this.lstColleague.TabIndex = 4;
+            this.lstColleague.UseCompatibleStateImageBehavior = false;
+            // 
+            // lstCars
+            // 
+            this.lstCars.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lstCars.HideSelection = false;
+            this.lstCars.Location = new System.Drawing.Point(274, 176);
+            this.lstCars.Name = "lstCars";
+            this.lstCars.Size = new System.Drawing.Size(253, 262);
+            this.lstCars.TabIndex = 5;
+            this.lstCars.UseCompatibleStateImageBehavior = false;
+            // 
+            // lstTank
+            // 
+            this.lstTank.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lstTank.HideSelection = false;
+            this.lstTank.Location = new System.Drawing.Point(535, 176);
+            this.lstTank.Name = "lstTank";
+            this.lstTank.Size = new System.Drawing.Size(253, 262);
+            this.lstTank.TabIndex = 6;
+            this.lstTank.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Medewerkers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Auto\'s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(532, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tankbeurt";
+            // 
+            // Landing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstTank);
+            this.Controls.Add(this.lstCars);
+            this.Controls.Add(this.lstColleague);
+            this.Controls.Add(this.btnTank);
+            this.Controls.Add(this.btnCars);
+            this.Controls.Add(this.btnColleague);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Landing";
+            this.Text = "Gasolin | Landing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnColleague;
+        private System.Windows.Forms.Button btnCars;
+        private System.Windows.Forms.Button btnTank;
+        private System.Windows.Forms.ListView lstColleague;
+        private System.Windows.Forms.ListView lstCars;
+        private System.Windows.Forms.ListView lstTank;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
